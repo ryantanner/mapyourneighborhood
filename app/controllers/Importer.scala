9e54ee6346274d9bf7f766ed2d383ba6f9fb296e
ryantanner <ryan.tanner@gmail.com>
2012-10-29 16:01:42 -0500

    Comet data push working in importer

--- app/controllers/Importer.scala
+++ app/controllers/Importer.scala
@@ -2,32 +2,37 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.libs.iteratee._
+import play.api.libs.Comet
+import play.api.libs.Comet.CometMessage
+import play.api.templates.Html
 
 object Importer extends Controller {
 
   def index = Action {
-    Ok(views.html.importer.index())
+    Ok(views.html.importer.index("MapYourNeighborhood Data Importer"))
   }
 
   def upload = Action(parse.multipartFormData) { request =>
     request.body.file("census-data").map { census =>
-      import java.io.{File}
+      import java.io.{File, FileInputStream}
       import io.Source
 
       val filename = census.filename
       val contentType = census.contentType
 
-      val stream:Iterator[String] = Source.fromFile(census.ref.file).getLines 
+      lazy val data = Enumerator.fromFile(census.ref.file, 192)
 
-      val firstLine = stream.next
+      val toCometMessage = Enumeratee.map[Array[Byte]] { data =>
+        Html("""<script>console.log('""" + data.toList.map(_.toChar).mkString.trim + """')</script>""")
+      }
 
-      Ok("file upload: " + firstLine)
+      Ok.stream(data.andThen(Enumerator.eof).through(toCometMessage)) 
     }.getOrElse{
       Redirect(routes.Importer.index).flashing(
         "error" -> "Missing file"
       )
     }
   } 
-
 
 }
