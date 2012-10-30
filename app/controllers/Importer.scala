1269e0838b42682a80301b0c85453f7bd244061b
ryantanner <ryan.tanner@gmail.com>
2012-10-30 10:37:36 -0500

    Data importer now streams Comet messages in response

--- app/controllers/Importer.scala
+++ app/controllers/Importer.scala
@@ -6,6 +6,11 @@ import play.api.libs.iteratee._
 import play.api.libs.Comet
 import play.api.libs.Comet.CometMessage
 import play.api.templates.Html
+import play.api.libs.json._
+
+import play.api.libs.concurrent._
+
+import akka.util.duration._
 
 object Importer extends Controller {
 
@@ -21,11 +26,16 @@ object Importer extends Controller {
       val filename = census.filename
       val contentType = census.contentType
 
-      lazy val data = Enumerator.fromFile(census.ref.file, 192)
+      Ok.stream(Enumerator("Processing \"" + filename + "\"...").andThen(Enumerator.eof))
 
-      val toCometMessage = Enumeratee.map[Array[Byte]] { data =>
-        Html("""<script>console.log('""" + data.toList.map(_.toChar).mkString.trim + """')</script>""")
+      lazy val data = Enumerator.fromFile(census.ref.file, 192).map { item => 
+        Promise.timeout(Some(censusToJson(item.toList.map(_.toChar).mkString.trim)), 100 milliseconds)
       }
+
+      val toCometMessage = Enumeratee.map[Promise[Some[JsValue]]] { data => data.value.fold( //{ value:Redeemed[Some[JsValue]] =>
+        onError => Html("ERROR"),
+        onSuceess => Html("""<script>parent.addEntryToTable('""" + data.value.get.get + """');</script>""")
+      )}
 
       Ok.stream(data.andThen(Enumerator.eof).through(toCometMessage)) 
     }.getOrElse{
@@ -34,5 +44,26 @@ object Importer extends Controller {
       )
     }
   } 
+
+  private def censusToJson(censusItem: String): JsValue = {
+
+    val item = censusItem.toList
+  
+    Json.toJson(
+      Map(
+        "uace"          -> item.slice(0,5).mkString.trim,
+        "name"          -> item.slice(10,70).mkString.trim,
+        "pop"           -> item.slice(76,84).mkString.trim,
+        "hu"            -> item.slice(90,98).mkString.trim,
+        "arealand"      -> item.slice(104,117).mkString.trim,
+        "arealandsqmi"  -> item.slice(123,131).mkString.trim,
+        "areawater"     -> item.slice(137,150).mkString.trim,
+        "areawatersqmi" -> item.slice(156,164).mkString.trim,
+        "popden"        -> item.slice(170,178).mkString.trim,
+        "lsadc"         -> item.slice(183,185).mkString.trim
+      )
+    )
+
+  }
 
 }
